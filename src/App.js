08912f811761ae2eb23d4f93b5f754314f3b630b
Kanishk Singh <kanishk.singh198@gmail.com>
2023-01-30 23:48:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Balance from "./Components/Balance";
+import NewTransaction from "./Components/NewTransaction";
+import IncomeExpense from "./Components/IncomeExpense";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">Expense Tracker</header>
+			<div className="container">
+				<div>
+					<Balance />
+				</div>
+				<div>
+					<IncomeExpense />
+				</div>
+				<div>
+					<NewTransaction />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Components/NewTransaction.js
@@ -0,0 +1,9 @@
+const NewTransaction = () => {
+	return (
+		<div>
+			<h4>New Transaction</h4>
+		</div>
+	);
+};
+
+export default NewTransaction;
